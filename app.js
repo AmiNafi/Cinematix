5e3e139a3794fd4b88848e55acf595755e9e1b10
AmiNafi <csenafiul@gmail.com>
2023-05-25 13:06:43 +0600

    Implemented Pagination

--- app.js
+++ app.js
@@ -28,8 +28,11 @@ app.get('/', async (req, res) => {
     res.render('home');
 })
 app.get('/movielist', async (req, res) => {
-    const movielist = await singleMovie.find({});
-    res.render('movielist/index', { movielist });
+    const { page = 1 } = req.query, lim = 21;
+    const totalmovies = await singleMovie.count();
+    console.log(totalmovies);
+    const movielist = await singleMovie.find({}).limit(lim).skip((page - 1) * lim);;
+    res.render('movielist/index', { data: { movielist, page, totalmovies, lim } });
 })
 app.get('/movielist/newmovie', async (req, res) => {
     res.render('movielist/newmovie');
@@ -51,14 +54,16 @@ app.post('/movielist', async (req, res) => {
 });
 app.put('/movielist/:id', async (req, res) => {
     const { id } = req.params;
+
+
     const curmovie = await singleMovie.findByIdAndUpdate(id, { ...req.body.movie });
-    res.redirect(`/movielist/${curmovie._id}`)
+    res.redirect(`/movielist/${curmovie._id}`);
 });
 app.delete('/movielist/:id', async (req, res) => {
     const { id } = req.params;
     await singleMovie.findByIdAndDelete(id);
     res.redirect('/movielist');
 });
-app.listen(3000, () => {
-    console.log('Serving on port 3000');
-})
+app.listen(8080, () => {
+    console.log('Serving on port 8080');
+});
